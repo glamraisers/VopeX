aa25e9f0d0d656ac5049322087d4ef716ee715d0
Glam Raisers <glamraisers@gmail.com>
2025-01-15 04:55:15 +0600

    Update
    v1

---
+++ src/components/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div className="settings-page">
+      <h1>Settings</h1>
+      <p>Manage your account settings here.</p>
+    </div>
+  );
+};
+
+export default Settings;
