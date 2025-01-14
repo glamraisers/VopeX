be6c9bed7f665d96f09bc07667ad87bb47f75eda
Glam Raisers <glamraisers@gmail.com>
2025-01-15 05:09:05 +0600

    Update
    v1

---
+++ src/utils/aiHelpers.js
@@ -0,0 +1,15 @@
+// aiHelpers.js
+export const preprocessData = (data) => {
+  // Implement data preprocessing logic
+  return data;
+};
+
+export const trainModel = (data) => {
+  // Implement model training logic
+  return data;
+};
+
+export const evaluateModel = (data) => {
+  // Implement model evaluation logic
+  return data;
+};
