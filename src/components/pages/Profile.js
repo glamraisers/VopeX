7c06dadec4fbdcf5b4c06f781426038bda9a119f
Glam Raisers <glamraisers@gmail.com>
2025-01-15 04:55:35 +0600

    Update
    v1

---
+++ src/components/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="profile-page">
+      <h1>Your Profile</h1>
+      <p>View and edit your profile information.</p>
+    </div>
+  );
+};
+
+export default Profile;
