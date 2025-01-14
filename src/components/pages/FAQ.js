743c8796502e84f8c46acfb59c22520075910741
Glam Raisers <glamraisers@gmail.com>
2025-01-15 04:56:00 +0600

    Update
    v1

---
+++ src/components/pages/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FAQ = () => {
+  return (
+    <div className="faq-page">
+      <h1>Frequently Asked Questions</h1>
+      <p>Find answers to common questions here.</p>
+    </div>
+  );
+};
+
+export default FAQ;
