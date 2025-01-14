655198aef60e2f4f5aa77288ad2ac76c42caa429
Glam Raisers <glamraisers@gmail.com>
2025-01-15 05:07:56 +0600

    Update
    v1

---
+++ src/utils/helpers.js
@@ -0,0 +1,18 @@
+// helpers.js
+export const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
+};
+
+export const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const generateRandomString = (length) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
