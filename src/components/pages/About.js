6b31c01285b5634531415fe7caabeb6ccf485b47
Glam Raisers <glamraisers@gmail.com>
2025-01-15 04:54:29 +0600

    Update
    v1

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About Vopex App</h1>
+      <p>Vopex App is designed to streamline your sales processes and enhance your productivity.</p>
+    </div>
+  );
+};
+
+export default About;
