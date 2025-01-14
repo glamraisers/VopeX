a081a1f4efa91af76a3b1f5eb9875957bfb8ebf5
Glam Raisers <glamraisers@gmail.com>
2025-01-15 04:53:58 +0600

    Update
    v1

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Vopex App</h1>
+      <p>Your ultimate sales management platform.</p>
+    </div>
+  );
+};
+
+export default Home;
