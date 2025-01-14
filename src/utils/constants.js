8d7aade5319efa13810541fbc3f9e40bac1a90b4
Glam Raisers <glamraisers@gmail.com>
2025-01-15 05:07:27 +0600

    Update
    v1

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+// constants.js
+export const API_URL = process.env.REACT_APP_API_URL;
+export const JWT_SECRET = process.env.REACT_APP_JWT_SECRET;
+export const DB_HOST = process.env.REACT_APP_DB_HOST;
+export const DB_PORT = process.env.REACT_APP_DB_PORT;
+export const DB_USER = process.env.REACT_APP_DB_USER;
+export const DB_PASSWORD = process.env.REACT_APP_DB_PASSWORD;
+export const DB_NAME = process.env.REACT_APP_DB_NAME;
