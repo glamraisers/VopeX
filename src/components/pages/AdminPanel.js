281a76da06168c8161134950e2b95eb7e07fea4e
Glam Raisers <glamraisers@gmail.com>
2025-01-15 04:56:23 +0600

    Update
    v1

---
+++ src/components/pages/AdminPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPanel = () => {
+  return (
+    <div className="admin-panel">
+      <h1>Admin Panel</h1>
+      <p>Manage your application settings and user roles here.</p>
+    </div>
+  );
+};
+
+export default AdminPanel;
